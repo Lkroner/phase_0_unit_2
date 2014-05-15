24767e1025eb23911c823b375878eb05e3f2e1f1
Lauren Kroner <lauren.kroner@gmail.com>
2014-05-15 12:09:22 -0700

    updated wk4 challenge 2

--- week_4/2_creative/make_a_homework_cheater/homework_cheater_spec.rb
+++ week_4/2_creative/make_a_homework_cheater/homework_cheater_spec.rb
@@ -1,9 +1,47 @@
 require_relative "my_solution"
 
-describe 'homework_cheater' do |variable|
-  let(:array_1) { [1, 2, 3, 4, 5, 5, 7] }
-  let(:array_2) { [4, 4, 5, 5, 6, 6, 6, 7] }
+describe 'essay_writer' do
+  let(:first_sentence) {["The Modern Astronomer", "Neil deGrasse Tyson", "1958", "He is an American astrophysicist, author, and science communicator. 
+  	He is currently the Frederick P. Rose Director of the Hayden Planetarium at the 
+  	Rose Center for Earth and Space and a research associate in the department of astrophysics at the American Museum of Natural History", male]}
+  let(:second_sentence) {["The Modern Astronomer", "France Córdova", "1947", "She is an American astrophysicist, researcher and university administrator. 
+  	As of March 2014, she is the director of the National Science Foundation. Previously, she was the eleventh President of Purdue University", female]}
+  let(:third_sentence) {["Earth's Comet", "Halley's Comet", "240 BC, when it was first observed", "It is the best-known of the short-period comets and is visible from Earth every 75–76 years. 
+  	Halley is the only short-period comet that is clearly visible to the naked eye from Earth, and the only naked-eye comet that might appear twice in a human lifetime.", thing]}
 
-
-  
+  it "is defined as a method" do
+    defined?(essay_writer).should eq 'method'
+  end
+  it "requires a five arguments" do
+    method(:essay_writer).arity.should eq 5
+  end
+  it "returns the grammatically correct sentence of the first sentence" do
+    essay_writer(first_sentence).should eq "The Modern Astronomer
+    	     Neil deGrasse Tyson was born in 1958. He changed the world and is someone we should all learn about further. 
+    	     He is an American astrophysicist, author, and science communicator. 
+  	         He is currently the Frederick P. Rose Director of the Hayden Planetarium at the 
+  	         Rose Center for Earth and Space and a research associate in the department of astrophysics at the American Museum of Natural History. 
+  	         Neil deGrasse Tyson is important to the world."
+  end
+  it "returns the grammatically correct sentence of the second sentence" do
+    essay_writer(second_sentence).should eq "The Modern Astronomer 
+    	     France Córdova was born in 1947. She changed the world and is someone we should all learn about further. 
+    	     She is an American astrophysicist, researcher and university administrator. 
+  	         As of March 2014, she is the director of the National Science Foundation. Previously, she was the eleventh 
+  	         President of Purdue University. France Córdova is important to the world."
+  end
+  it "returns the grammatically correct sentence of the third sentence" do
+    essay_writer(third_sentence).should eq "Earth's Comet
+    	     Halley's Comet was important since 240 BC, when it was first observed. It changed the world and is something we should all learn about further. 
+    	     It is the best-known of the short-period comets and is visible from Earth every 75–76 years. 
+  	         Halley is the only short-period comet that is clearly visible to the naked eye from Earth, 
+  	         and the only naked-eye comet that might appear twice in a human lifetime.. 
+  	         Halley's Comet is important to the world."
+  end
 end
+
+
+
+
+
+
--- week_4/2_creative/make_a_homework_cheater/my_solution.rb
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -1,17 +1,44 @@
 # U2.W4: Homework Cheater!
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
-# Input: 
-# Output:
-# Steps:
+# define method essay_writer
+#        essay_writer will take parameters: title, topic, date, thesis, pronoun
+#           if pronoun = male use "he" template
+#           else use "she" template
+#        insert title, topic, date and thesis parameters as strings into template.
+#        print out output essay
+#        end
+
+# Input: Parameters: title, topic, date, thesis, pronoun
+# Output: Essay string.
+# Steps: 
+
+# Take parameters
+# Choose between "he" or "she" template
+# Insert parameters
+# Print finished essay
 
 
 # 3. Initial Solution
 
+def essay_writer(title, topic, date, thesis, pronoun)
+    if pronoun == female
+       puts "#{title}
+    	     #{topic} was born in #{date}. She changed the world and is someone we should all learn about further. 
+    	     #{thesis}. #{topic} is important to the world."
+    if pronoun == male
+       puts "#{title}
+    	     #{topic} was born in #{date}. He changed the world and is someone we should all learn about further. 
+    	     #{thesis}. #{topic} is important to the world."
+    else 
+       puts "#{title}
+    	     #{topic} was important since #{date}. It changed the world and is something we should all learn about further. 
+    	     #{thesis}. #{topic} is important to the world."
+end
 
 
 
