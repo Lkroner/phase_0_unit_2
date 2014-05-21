b96c907dfcbaba283d49f5f97570fd224aef20e8
Lauren Kroner <lauren.kroner@gmail.com>
2014-05-21 14:29:15 -0700

    completed challenge 1

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -5,41 +5,95 @@
 
 # 2. Pseudocode
 
-# Input:
+# Input: Number of sides of the die
 # Output:
+# sides output = 
+# roll output = a number 1 through the number of sides
+
 # Steps:
+
+# define class Die
+#  give it three methods
+#        1. use attribute accessor method to be able to read/write sides input
+#        2. define method initialize that takes the parameter sides
+#      			create instance variable sides that stores the sides parameter.
+#               unless the sides parameter is less than 1
+#					then puts "Side number must an integer be more than 0."
+
+#        3. define method roll
 
 
 # 3. Initial Solution
 
 class Die
+  attr_accessor :sides	
+
   def initialize(sides)
-    # code goes here
-  end
-  
-  def sides
-    # code goes here
+    @sides = sides 
+	    unless sides > 1
+	    raise ArgumentError.new("Must use number more than 1")
+	  end
   end
   
   def roll
-    # code goes here
+	1 + rand(sides)
   end
+
 end
 
 
 
 # 4. Refactored Solution
 
-
-
-
+# 
+#   
+#   
+#  
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+die = Die.new(6)
+p die.sides 
+p die.roll
 
-
-
-
+die = Die.new(2)
+p die.sides 
+p die.roll
 
 # 5. Reflection 
+
+# * What parts of your strategy worked? What problems did you face?
+# Most parts of my strategy worked! The only thing that tripped me up a bit was I had the "raise"
+# argument in the wrong method. Once I looked over the tests I noticed that it had to be in the 
+# initialize method and fixed that.
+
+# * What questions did you have while coding? What resources did you find to help you answer them?
+# How to randomize a number 1 through n. I used this stack overflow response: 
+# http://stackoverflow.com/questions/198460/how-to-get-a-random-number-in-ruby
+
+
+# * What concepts are you having trouble with, or did you just figure something out? If so, what?
+# Didn't have much problem with this challenge.
+
+
+# * Did you learn any new skills or tricks?
+# The attribute method! Fun one to use.
+
+
+# * How confident are you with each of the Learning Competencies?
+# Break down problems into implementable pseudocode(75%)
+# Implement a basic Ruby class and identify when to use instance variables(75%)
+# Use if/else statements, string methods, while/until loops, Enumerable#each methods(75%)
+# Find and use built-in Ruby methods to solve challenges(75%)
+# Explain how instance variables and methods represent the characteristics and actions of an object.(80%)
+
+
+# * Which parts of the challenge did you enjoy?
+# I enjoyed learning about the attribute method family. It's still a bit confusing to me but it's a very cool
+# way to clean up my code.
+
+# * Which parts of the challenge did you find tedious?
+# Nothing super tedious here... except the pseudocode as usual.
+
+
